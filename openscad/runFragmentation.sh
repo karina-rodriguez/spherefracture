2b80030eda3d3fab73bfcfd44c043693ca3b0cb2
PhotoScan <photoscan@sox.local>
2018-12-13 21:21:21 +0000

    run Fragment script not working ...

--- openscad/runFragmentation.sh
+++ openscad/runFragmentation.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 
 declare -i NUM_PARTS=12
-NUM_PARTS=$(( $NUM_PARTS + 2 ))
 echo "$NUM_PARTS"
 
-#while [ $NUM_PARTS > 0 ]
-#do
 
-#echo "1"
-#	echo "Hello, world $NUM_PARTS!"
-#	NUM_PARTS=($NUM_PARTS-1)
-#done
