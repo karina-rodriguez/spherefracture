1a3da858a4b8bd2e4894bc62dfea18980e2da98c
PhotoScan <photoscan@sox.local>
2019-01-08 09:55:33 +0000

    changes added to run values for different versions of same puzzle

--- openscad/runPuzzleGenerator.py
+++ openscad/runPuzzleGenerator.py
@@ -3,23 +3,27 @@ import os
 import random
 import subprocess
 
-totalexamples=5
+totalexamples=2
 numparts=0
-#thefiles=['ambercuppoisson_100K.stl']
-#thefiles=['tocutUSETHISFORPUZZLE_500K_poisson.stl']
-thefiles=['elephantreconstructedsmoothclean125KWith7mmthickness.stl']
+settings=[[6, 6, 0.3, 0.1, 0.85, 0.0,'standard'],[6, 6, 0.3, 0.1, 0.85, 1.0,'standard_smooth'],[7, 1, 0.1, 0.1, 0.5, 0.0,'low_niter'],[4, 4, 0.5, 0.1, 0.5, 1.0,'low_amplitude'],[4, 4, 0.5, 0.6, 0.5, 1.0,'high_amplitude']]
+thefiles=['saltdeanpot_30K.stl']
+#thefiles=['ambercup_smooth','saltdeanpot_smooth','elephant_smooth']
 
+filenum=1
 for file in thefiles:
 	print(file)
 	# create 2 example of puzzle fragmented spheres
-	for x in range(0, totalexamples):
+	#for x in range(0, totalexamples):
+	for puzzleset in settings:
 		generatedpiece=0;
 		while(generatedpiece==0):
 			# do between 7 and 14 parts
 			#numparts = random.randint(7,14)
-			numparts = random.randint(5,10)
-			print('Puzzle '+str(x)+' with '+str(numparts)+' parts')
-			breakSphereprogram='/Users/photoscan/workspace/breakSphere/source/breakSphere -numparts '+str(numparts)
+			numparts = 10
+			seedLine = 22
+			seedOrientation = 7
+			print('Puzzle '+str(puzzleset[6])+' with '+str(numparts)+' parts')
+			breakSphereprogram='/Users/photoscan/workspace/breakSphere/source/breakSphere -numparts '+str(numparts)+' -seedLine '+str(seedLine)+' -seedOrientation '+str(seedOrientation)+' -m '+str(puzzleset[0])+' -niter '+str(puzzleset[1])+' -jitter '+str(puzzleset[2])+' -amplitude '+str(puzzleset[3])+' -decay '+str(puzzleset[4])+' -smoothing '+str(puzzleset[5])
 			command_run = subprocess.call(breakSphereprogram,shell=True)
 			if command_run == 0:
 				print "Its worked!!"
@@ -29,11 +33,12 @@ for file in thefiles:
 
 
 		#for each fragment piece, intersect with the 3D model
-		resultfolder='result/'+file+'_puzzle'+str(x)
+		resultfolder='result/'+file+'_puzzle_'+str(puzzleset[6])
 		os.system('mkdir '+resultfolder)	
 		for piece in range (0, numparts):
 			print("I will break the piece: "+str(piece))
 			os.system('cp fragmentsphere/fragment_'+str(piece)+'.stl '+resultfolder+'/')
-			openscadcmd = 'openscad -D \'file="geometry/'+file+'"\' -D num='+str(piece)+' -o '+resultfolder+'/puzzlepiece_'+str(piece)+'of'+str(numparts)+'.stl puzzlepieces.scad'
+			openscadcmd = 'openscad -D \'file="geometry/'+file+'"\' -D num='+str(piece)+' -D geo='+str(filenum)+' -o '+resultfolder+'/puzzlepiece_'+str(piece)+'of'+str(numparts)+'.stl puzzlepieces.scad'
 			print(openscadcmd)
 			os.system(openscadcmd) 
+	filenum=filenum+1
